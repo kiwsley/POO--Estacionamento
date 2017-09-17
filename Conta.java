6efa70d6a319f9aacc3e9bb2284f61a656f45135
kiwsley <31630113+kiwsley@users.noreply.github.com>
2017-09-17 10:28:33 -0300

    Atualização1.2

---
+++ Conta.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+/**
+ *
+ * @author Usuário
+ */
+public class Conta {
+    private float taxaFixa;
+    private float taxaPorHora;
+    private LocalDateTime dataEntrada;
+    private Carro carro;
+    private boolean aberta;     // Bool para dizer se a conta esta aberta ou fechada
+    
+    
+    public Conta()
+    {
+        
+    }
+    
+    
+    // Calcula quantidade de horas da chegada do carro até a saída,
+    // multiplica pela taxa por hora e soma à taxa fixa
+    public float calculaConta(LocalDateTime dataSaida)
+    {
+        //calcula diferença entre as horas de chegada e saída
+        long meses = dataEntrada.until(dataSaida, ChronoUnit.HOURS);
+        
+        return meses*taxaPorHora + taxaFixa;
+    }
+    
+    public Carro getCarro()
+    {
+        return this.carro;
+    }
+    
+    public void abrirConta(LocalDateTime dataEntrada, Carro carro)
+    {
+        this.dataEntrada = dataEntrada;
+        this.taxaFixa = 2;                  // Taxa fixa = 2 reais
+        this.carro = carro;
+        this.aberta = true;
+        this.taxaPorHora = 2;       // + 2 reais por hora
+    }
+    
+    public void fecharConta()
+    {
+        this.aberta = false;
+    }
+}
